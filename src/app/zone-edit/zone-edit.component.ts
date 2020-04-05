27dcbc63f8ff59167b60f43b9e20a5d095c9f4cb
fsalahuddin <farjana.salahuddin@senseta.com>
2020-04-05 21:45:15 +0300

    Edit Schedule Page Initial design

--- src/app/zone-edit/zone-edit.component.ts
+++ src/app/zone-edit/zone-edit.component.ts
@@ -10,15 +10,35 @@ export class ZoneEditComponent implements OnInit {
 
   public schedules;
   public sortedSchedules;
+  public allZones = [];
 
   constructor(public scheduleService: ScheduleService) { }
 
   ngOnInit() {
+        this.getAllZones();
+        console.log(this.scheduleService.setTempUnit);
         if (!this.scheduleService.setTempUnit) {
             this.scheduleService.setTempUnit = 'C';
         }
         this.schedules = this.scheduleService.schedules.sort((a, b) => b.date.getTime() - a.date.getTime());
         this.sortedSchedules = this.schedules;
+        console.log(this.sortedSchedules);
+  }
+
+  public getAllZones() {
+        this.scheduleService.getAllZones()
+        .subscribe(res => this.allZones = res );
+  }
+
+  public getZoneName(id) {
+        const retrievedZone = this.allZones.filter(zone => zone.id === id)[0];
+        if (retrievedZone) {
+          return retrievedZone.name;
+        } else { return ' -- '; }
+  }
+
+  public createSchedule() {
+        console.log('trigger new schedule');
   }
 
 
